e414fc79e9fe6de9b953b133db43579b750fa72c
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-02-10 16:58:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function updateObjectWithKeyAndValue(recipes, key, value){
 
 
 function updateObjectWithKeyAndValue(recipes, key, value){
-  Object.assign({}, recipes, {[key]: value}); 
-  return 
+  Object.assign({}, recipes, {key: value}); 
+}
