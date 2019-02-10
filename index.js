0e0919f78592d3665cdb858c0c710fc10ead7acd
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-02-10 17:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes ={flour: "1 cup"};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, {[key]: value})
+  return Object.assign({}, object, {[key]: value})
 }
 
 
